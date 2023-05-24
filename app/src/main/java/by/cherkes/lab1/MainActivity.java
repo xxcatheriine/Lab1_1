f607cdb8b0a7c8930f9cdbb6243360141d856e06
Черкес Екатерина <e.cherkes@21vek.local>
2023-05-24 13:15:03 +0300

    Initial commit

---
+++ app/src/main/java/by/cherkes/lab1/MainActivity.java
@@ -0,0 +1,14 @@
+package by.cherkes.lab1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    //TODO add new method
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
